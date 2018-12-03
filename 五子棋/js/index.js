0ad5906fd041e5783fc65cb117fc703754f5c2b9
gfyDemo <1053221101@qq.com>
2018-12-03 16:58:37 +0800

    开始吧

---
+++ 五子棋/js/index.js
@@ -0,0 +1,3 @@
+const canvas = document.getElementById('canvas')
+let canvas_width = canvas.clientWidth = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth
+let canvas_height = canvas.clientHeight = document.documentElement.clientHeight || document.body.clientHeight || window.innerHeight
