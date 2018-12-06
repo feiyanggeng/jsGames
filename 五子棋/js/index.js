1cb32dc769705245d42eec7115eef368f272ff24
gfyDemo <1053221101@qq.com>
2018-12-06 15:05:29 +0800

    helllo

--- 五子棋/js/index.js
+++ 五子棋/js/index.js
@@ -1,3 +1,7 @@
 const canvas = document.getElementById('canvas')
-let canvas_width = canvas.clientWidth = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth
-let canvas_height = canvas.clientHeight = document.documentElement.clientHeight || document.body.clientHeight || window.innerHeight
+let ctx = canvas.getContext('2d')
+let canvas_width = canvas.width = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth
+let canvas_height = canvas.height = document.documentElement.clientHeight || document.body.clientHeight || window.innerHeight
+const image = new Image()
+image.src = '/image/geng.gif'
+ctx.drawImage(image, 10, 10)
