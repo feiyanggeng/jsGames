1cb3f63b551256cea6afdd19a3b25f17a3dbac2d
耿飞阳 <1053221101@qq.com>
2019-01-23 15:14:02 +0800

    赢法数组 理清

--- 五子棋/js/index.js
+++ 五子棋/js/index.js
@@ -10,6 +10,12 @@ image.onload = function() {
 
 let isme = true                // 判断是自己还是电脑
 let chess = []                  // 记录棋盘上面的点
+let wins = []                   // 保存所有赢法
+let count = 0                   // 储存赢法的个数
+
+// 赢法统计数组
+let myMins = []
+let computer = []
 
 // 初始化棋盘数组
 for (let i=0; i < 15; i++) {
@@ -17,6 +23,52 @@ for (let i=0; i < 15; i++) {
         for (let j = 0; j < 15; j++) {
                 chess[i][j] = 0
         }
+}
+
+/**
+ *  初始化所有赢法数组
+*/
+// 初始化 横向的所有赢法
+for (let i = 0; i < 15; i++ ) {
+        for (let j = 0; j < 11; j++) {
+                for (let k = 0; k < 5; k++) {
+                        wins[i][j+k][count] = true
+                }
+        count++
+        }
+}
+// 初始化 纵向的所有赢法
+for (let i = 0; i < 15; i++ ) {
+        for (let j = 0; j < 11; j++) {
+                for (let k = 0; k < 5; k++) {
+                        wins[j+k][i][count] = true
+                }
+        count++
+        }
+}
+// 初始化 左斜线的所有赢法
+for (let i = 0; i < 11; i++ ) {
+        for (let j = 0; j < 11; j++) {
+                for (let k = 0; k = 4; k++) {
+                        wins[i+k][j+k][count] = true
+                }
+        count++
+        }
+}
+// 初始化 右斜线的所有赢法
+for (let i = 0; i < 11; i++ ) {
+        for (let j = 14; j > 3; j--) {
+                for (let k = 0; k = 4; k++) {
+                        wins[i+k][j-k][count] = true
+                }
+        count++
+        }
+}
+
+// 初始化赢法统计数组
+for(let i = 0; i < count; i++) {
+        myMins[i] = 0
+        computer[i] = 0
 }
 
 // 绘制棋盘
@@ -40,15 +92,15 @@ function drawChessPieces(i, j, me) {
         if (me) {
                 color.addColorStop(0, "#0A0A0A")
                 color.addColorStop(1, "#636766")
-                chess[i][j] = 1
+                chess[i][j] = 1                         // 玩家 落子设置为 1
         } else {
                 color.addColorStop(0, "#D1D1D1")
                 color.addColorStop(1, "#F9F9F9")
-                chess[i][j] = 2
+                chess[i][j] = 2                         // 电脑 落子设置为 2
         }
         ctx.fillStyle = color
         ctx.fill()
-        isme = !isme
+        isme = !isme                                    // 转换落子角色
 }
 
 // 玩家绘制
@@ -67,5 +119,6 @@ canvas.onclick = function(e) {
 
 // 电脑绘制
 computerAI = function () {
+        
         drawChessPieces(i, j, isme)
 }
