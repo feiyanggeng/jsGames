32599c4e145b4251279fbb58b24024d4faa8bd0a
耿飞阳 <1053221101@qq.com>
2019-01-23 16:54:24 +0800

    初始化所有赢法

--- 五子棋/js/index.js
+++ 五子棋/js/index.js
@@ -25,6 +25,14 @@ for (let i=0; i < 15; i++) {
         }
 }
 
+
+//初始化wins
+for(var i = 0; i < 15; i++){
+        wins[i] = [];
+        for(var j = 0; j < 15; j++) {
+            wins[i][j] = [];
+        }
+}
 /**
  *  初始化所有赢法数组
 */
@@ -34,7 +42,7 @@ for (let i = 0; i < 15; i++ ) {
                 for (let k = 0; k < 5; k++) {
                         wins[i][j+k][count] = true
                 }
-        count++
+                count++
         }
 }
 // 初始化 纵向的所有赢法
@@ -49,7 +57,7 @@ for (let i = 0; i < 15; i++ ) {
 // 初始化 左斜线的所有赢法
 for (let i = 0; i < 11; i++ ) {
         for (let j = 0; j < 11; j++) {
-                for (let k = 0; k = 4; k++) {
+                for (let k = 0; k < 5; k++) {
                         wins[i+k][j+k][count] = true
                 }
         count++
@@ -58,7 +66,7 @@ for (let i = 0; i < 11; i++ ) {
 // 初始化 右斜线的所有赢法
 for (let i = 0; i < 11; i++ ) {
         for (let j = 14; j > 3; j--) {
-                for (let k = 0; k = 4; k++) {
+                for (let k = 0; k < 4; k++) {
                         wins[i+k][j-k][count] = true
                 }
         count++
